e0b0cba6aba57c7dd8f5b6cd64b2bd550913bb55
Aliaksandr Okrul <okrul@list.ru>
2021-03-15 20:38:03 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  let newstr = '';
+  for (i = str.length; i > 0; i--)
+  {
+    newstr = newstr + str[i -1];
+    } 
+
+    return newstr;
 }
